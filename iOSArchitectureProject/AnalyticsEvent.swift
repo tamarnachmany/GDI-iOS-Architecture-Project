93903fccc2f08f351c914bf0effd480fa1a3f22a
tamarnachmany <tamarnachmany@gmail.com>
2017-05-10 18:19:01 -0400

    Make view controller with button for students to refactor

---
+++ iOSArchitectureProject/AnalyticsEvent.swift
@@ -0,0 +1,9 @@
+//
+//  AnalyticsEvent.swift
+//  iOSArchitectureProject
+//
+//  Created by Tamar Nachmany on 5/9/17.
+//  Copyright © 2017 Girl Develop It. All rights reserved.
+//
+
+import Foundation
---
+++ iOSArchitectureProject/ButtonAnalyticsEventHandling.swift
@@ -0,0 +1,9 @@
+//
+//  ButtonAnalyticsEventHandling.swift
+//  iOSArchitectureProject
+//
+//  Created by Tamar Nachmany on 5/7/17.
+//  Copyright © 2017 Girl Develop It. All rights reserved.
+//
+
+import Foundation
